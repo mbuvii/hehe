6f4e27b7ab08055be5c9f8822351430cd234a9d5
MBUVI DAVID <147725809+cheekydavy@users.noreply.github.com>
2024-08-29 21:06:15 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/a7ca8d4777fa8ad977795.jpg",
+    ALIVE_MSG: process.env.ALIVE_MSG || "Yoh. I am Savage Md created by Mbuvi Tech.",
 };
