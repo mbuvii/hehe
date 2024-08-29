d4eeb067a18c8ddb8f4ad6754542e672b13a9059
MBUVI DAVID <147725809+cheekydavy@users.noreply.github.com>
2024-08-29 09:56:50 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const {
+ default: makeWasocket,
+ useMultiFileAuthState,
+  DisconnectReason,
+  jidNormalizedUser,
+  getContentType,
+  fetchLatestBaileysVersion,
+  Browsers
+} = require ('@whiskeysocketbaileys/baileys')
+const { getbuffer, getgroupadmins, get
