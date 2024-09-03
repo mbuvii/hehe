43eb0f9cca24d6c88dae0bc3849897cd795e5e85
MBUVI DAVID <147725809+cheekydavy@users.noreply.github.com>
2024-09-03 11:01:37 +0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,4 +1,4 @@
-const config = require('../config')
+const {readEnv} = require('../lib/database')
 const {cmd , commands} = require('../command')
 
 cmd({
@@ -9,6 +9,7 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+ const config = await readEnv();   
 let menu = {
 main: '',
 download: '',
@@ -20,7 +21,7 @@ search: ''
 
 for (let i = 0; i < commands.length; i++) {
 if (commands[i].pattern && !commands[i].dontAddCommandList) {
-menu[commands[i].category] += `.${commands[i].pattern}\n`;
+menu[commands[i].category] += `${config.PREFIX}${commands[i].pattern}\n`;
  }
 }
 
@@ -51,7 +52,7 @@ ${menu.search}
   
  POWERED BY MBUVI TECH 
 '
-await conn.sendMessage(from,{image:{url:"https://telegra.ph/file/a7ca8d4777fa8ad977795.jpg"},caption:madeMenu},{quoted:mek})
+await conn.sendMessage(from,{image:{url:config.ALIVE_IMG},caption:madeMenu},{quoted:mek})
   
 }catch(e){
 console.log(e)
