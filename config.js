f3844163abab430482799ae736511ddca956731e
MBUVI DAVID <147725809+cheekydavy@users.noreply.github.com>
2024-09-03 08:53:33 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/a7ca8d4777fa8ad977795.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "Yoh. I am Savage Md created by Mbuvi Tech.",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
+MONGODB: process.env.MONGODB || "enter mongodb here",
 };
