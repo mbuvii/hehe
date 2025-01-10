e9f164b27c74bce1f78a2e7740888bd531279909
mbuvii <143305956+mbuvii@users.noreply.github.com>
2025-01-10 09:23:06 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "hu00WI7L#ehUvwi2sq_iBNGUdSpYoqfN15LxonFX1ZpHqAvaL_S8",
 MONGODB: process.env.MONGODB || "mongodb+srv://cheekyheroku:elisa1@elisa.sqrwy.mongodb.net/",
 };
