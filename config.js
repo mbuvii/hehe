fb5aaa335f96ad7726e47c81efaa16ca3d370e2a
mbuvii <143305956+mbuvii@users.noreply.github.com>
2025-01-11 11:05:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,7 @@
 const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+require('dotenv').config();
 
-function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
-}
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "hu00WI7L#ehUvwi2sq_iBNGUdSpYoqfN15LxonFX1ZpHqAvaL_S8",
-MONGODB: process.env.MONGODB || "mongodb+srv://cheekyheroku:elisa1@elisa.sqrwy.mongodb.net/",
+    SESSION_ID: process.env.SESSION_ID || "",
+    MONGODB: process.env.MONGODB || "enter mongodb here",
 };
