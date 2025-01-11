69d6d3fe3d3be9bc3d9e195807866ce42eec6af7
mbuvii <143305956+mbuvii@users.noreply.github.com>
2025-01-11 11:56:03 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
+// config.js
+require('dotenv').config(); // Ensure dotenv is loaded first
 
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID,
-  MONGODB: process.env.MONGODB,
-  PORT: process.env.PORT || 8000,
+    SESSION_ID: process.env.SESSION_ID,
+    MONGODB: process.env.MONGODB || "mongodb+srv://cheekyheroku:elisa1@elisa.sqrwy.mongodb.net/", // Ensure you have a default value here if needed
 };
